376bcad1f4c405b9a5fb3a76549f605a7c5e43f2
rick <rick.chang@klook.com>
2020-05-26 16:30:19 +0800

    Go:generate-parentheses

--- 22.generate-parentheses.go
+++ 22.generate-parentheses.go
@@ -6,23 +6,25 @@
 
 // @lc code=start
 func generateParenthesis(n int) []string {
+
 	var list []string
 	_gen(&list, 0, 0, n, "")
 	return list
 }
 
-func _gen(list *[]string, left, right, n int, current string){
+func _gen(list *[]string, left, right, n int, current string) {
 	if left == n && right == n {
-		list = append(*list, current)
-		return 
+		*list = append(*list, current)
+		return
 	}
 
 	if left < n {
-		_gen(list, left + 1, right, n, current+"(")
+		_gen(list, left+1, right, n, current+"(")
 	}
 	if right < n && left > right {
 		_gen(list, left, right+1, n, current+")")
 	}
 }
+
 // @lc code=end
 
